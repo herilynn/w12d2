bf89e1b46243e9259cf6373f9b5d454e80dd3e94
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-23 15:14:23 -0400

    completed frontend setup

--- authenticate_backend/frontend/src/index.js
+++ authenticate_backend/frontend/src/index.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
 const store = configureStore()
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 function Root() {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <BrowserRouter>
-      <App/>
+        <App />
       </BrowserRouter>
     </Provider>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
